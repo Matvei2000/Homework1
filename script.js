441b7b44a1ebd254180c2622a70b90c463994a40
Matvei2000 <1012mn2000@mail.ru>
2025-02-23 22:08:53 +0300

    Доработал дз

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-// let a = 10;
-// alert(a);
-// a = 20;
-// alert(a);
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
 
-// let iphoneReleaseYear = 2007;
-// alert(iphoneReleaseYear);
+let iphoneReleaseYear = 2007;
+alert(iphoneReleaseYear);
 
-// let creatorName = "Brendan Eich";
-// alert(creatorName);
+let creatorName = "Brendan Eich";
+alert(creatorName);
 
-// let num1 = 10;
-// let num2 = 2;
-// alert(num1 + num2);
-// alert(num1 - num2);
-// alert(num1 * num2);
-// alert(num1 / num2);
+let num1 = 10;
+let num2 = 2;
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
 
-// let result = 2 ** 5;
-// alert(result);
+let result = 2 ** 5;
+alert(result);
 
-// a = 9;
-// let b = 2;
-// let remainder = a % b;
-// alert(remainder);
+a = 9;
+let b = 2;
+let remainder = a % b;
+alert(remainder);
 
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num++;
-// num--;
-// alert(num);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
 
-// let age = prompt("Сколько вам лет?");
-// alert("Вам " + age + " лет!");
+let age = prompt("Сколько вам лет?");
+alert("Вам " + age + " лет!");
 
-// let user = {
-//     name: "Матвей",
-//     age: 23,
-//     isAdmin: true
-// };
-// console.log(user);
+let user = {
+    name: "Матвей",
+    age: 23,
+    isAdmin: true
+};
+console.log(user);
 
-// let userName = prompt("Как вас зовут?");
-// alert(`Привет, ${userName}!`);
+let userName = prompt("Как вас зовут?");
+alert(`Привет, ${userName}!`);
 
 
 
-// let number = prompt("Загадайте любое число");
-// number = parseFloat(number);
-// let doubled = number * 2;
-// alert(doubled);
+let number = prompt("Загадайте любое число");
+number = parseFloat(number);
+let doubled = number * 2;
+alert(doubled);
 
-// let addedTen = doubled + 10;
-// alert(addedTen);
+let addedTen = doubled + 10;
+alert(addedTen);
 
-// let dividedByTwo = addedTen / 2;
-// alert(dividedByTwo);
+let dividedByTwo = addedTen / 2;
+alert(dividedByTwo);
 
-// let finalResult = dividedByTwo - number;
-// alert(finalResult);
+let finalResult = dividedByTwo - number;
+alert(finalResult);
 
-// alert("5");
+alert("5");
 
 
 
@@ -92,9 +92,9 @@ if (d > 100 || e > 100) {
 }
 
 
-let a = '2';
-let b = '3';
-console.log(Number(a) + Number(b));
+let x = '2';
+let y = '3';
+console.log(Number(x) + Number(y));
 
 
 let monthNumber = 12;
